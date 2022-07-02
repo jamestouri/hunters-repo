0b544b0f0b9bc8a49ac38c238b15de7e14d515f9
jamestouri <j_touri@yahoo.com>
2022-07-02 10:58:03 -0700

    test

--- hunters/settings.py
+++ hunters/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'hunters.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/public/'],
+        'DIRS': ['public/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
