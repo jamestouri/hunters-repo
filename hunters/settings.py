82dbb0419b50d70642b021114d0c256411076b67
jamestouri <j_touri@yahoo.com>
2022-07-02 16:04:16 -0700

    Check mimtypes

--- hunters/settings.py
+++ hunters/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 import django_heroku
+import mimetypes
 
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
