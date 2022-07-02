0abd93cfed7791e548d57594892ff001479fbc33
jamestouri <j_touri@yahoo.com>
2022-07-02 10:46:32 -0700

    minor mistake

--- hunters/settings.py
+++ hunters/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'hunters.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/public/index.html'],
+        'DIRS': ['/public/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
