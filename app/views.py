4d0dc10d9572312e0debf7fd948201c7b1d2944b
jamestouri <j_touri@yahoo.com>
2022-07-02 16:34:59 -0700

    Begin split of backend and frontend

--- app/views.py
+++ app/views.py
@@ -10,21 +10,7 @@ from django.http.response import JsonResponse
 from .models import Activity, Coupon, Profile, Bounty, WorkSubmission, Transaction, CompletedBounty
 from rest_framework import status
 from rest_framework.decorators import api_view
-from django.views import View
-from django.http import HttpResponse, HttpResponseNotFound
 import os
-
-def index(request):
-    return render(request, 'index.html')
-
-class Assets(View):
-    def get(self, _request, filename):
-        path = os.path.join(os.path.dirname(__file__), 'static', filename)
-        if os.path.isfile(path):
-            with open(path, 'rb') as file:
-                return HttpResponse(file.read(), content_type='application/javascript')
-        else:
-            return HttpResponseNotFound()
 
 @api_view(['GET', 'POST'])
 def profiles(request):
