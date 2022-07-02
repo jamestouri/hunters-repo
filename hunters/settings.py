eff7d026cf66157ba4c93ea38f49811dfd759393
jamestouri <j_touri@yahoo.com>
2022-07-02 16:54:46 -0700

    Checking is environ doing something different

--- hunters/settings.py
+++ hunters/settings.py
@@ -30,7 +30,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','un-wall.herokuapp.com', 'unwall.xyz']
 
