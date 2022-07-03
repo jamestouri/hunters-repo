5be44e07bb4db608c18e4327adc282acc67a2a41
jamestouri <j_touri@yahoo.com>
2022-07-03 14:25:11 -0700

    Debug to False

--- hunters/settings.py
+++ hunters/settings.py
@@ -30,7 +30,10 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+# Not sure why this keeps putting true on Production despite 
+# having a env var on Heroku labelling False 
+# if needs True then switch it back
+DEBUG = False 
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','unwall-www.vercel.app', 'unwall.xyz']
 
