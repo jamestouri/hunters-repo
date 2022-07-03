a44f7a80e081e4197338dc60dd61683136ecaee3
jamestouri <j_touri@yahoo.com>
2022-07-02 21:14:56 -0700

    Add vercel

--- hunters/settings.py
+++ hunters/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','un-wall.herokuapp.com', 'unwall.xyz']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','unwall-www.vercel.app', 'unwall.xyz']
 
 
 # Application definition
